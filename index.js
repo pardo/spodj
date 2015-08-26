5a5e4882f8b8ddfcf81b4a8966937bd22f9f4ca7
Andres Pardini <unpardos@gmail.com>
2015-08-25 23:09:09 -0300

    stop player wip

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var lame = require('lame');
 var Speaker = require('speaker');
 var Spotify = require('spotify-web');
-//var uri = process.argv[2] || 'spotify:track:6tdp8sdXrXlPV6AZZN2PE8';
+
+//var uri = "spotify:track:4uB28m7RAflobYpnLMb6A2"
+//var uri = "spotify:track:5XhMeCYrRhQjL4sUoOmUCE"
 
 // Spotify credentials...
 var login = require('./login.js');
@@ -21,38 +23,52 @@ var password = login.password;
 
 
 
+SpotifyPlayer = (function(){
+  var that = this;
+  this.initialized = false;
+  this.spotifyStream = null;
+  this.speakerStream = null;
 
-var spotify = Spotify.login(username, password, function (err, spotify) {
-  if (err) throw err;
-});
-var currentTrack = null;
+  this.spotify = Spotify.login(username, password, function (err, spotify) {
+    if (err) throw err;
+    this.initialized = true;
+    console.log("Spotify Initialized");
+  });
 
+  this.stopPlayback = function(){
+    if (this.spotifyStream) {
+      spotifyStream.abort();
+      //spotifyStream.pause();
+    }
+    
+    //if (this.speakerStream) { speakerStream.close() }
+  };
 
-app.get('/api/', function (req, res) {
-  res.send('Hello World!');
-});
+  this.playTrackUri = function(uri){
+    console.log("Querying %s", uri);
+    this.spotify.get(uri, function (err, track) {
+      if (err) throw err;
+      console.log('Playing: %s - %s', track.artist[0].name, track.name);
+      // play() returns a readable stream of MP3 audio data
+      that.stopPlayback();
+      that.spotifyStream = track.play();
+      that.speakerStream = that.spotifyStream.pipe(new lame.Decoder()).pipe(new Speaker());
+      //.on('finish', function () {
+      //  spotify.disconnect();
+      //});
+
+    });
+  };
+
+  return this;
+})();
 
 
 app.post('/play/', function(req, res){
   var uri = req.body.uri;
-  spotify.get(uri, function (err, track) {    
-    if (err) throw err;
-    res.send("OK");
-    //if (currentTrack) { currentTrack.abort() }
-    
-    currentTrack = track;
-    console.log('Playing: %s - %s', track.artist[0].name, track.name);
-    // play() returns a readable stream of MP3 audio data
-    track.play()
-      .pipe(new lame.Decoder())
-      .pipe(new Speaker());
-      //.on('finish', function () {
-      //  spotify.disconnect();
-      //});
-  });
+  SpotifyPlayer.playTrackUri(uri);
+  res.send("OK");
 });
-
-
 
 
 var server = app.listen(8080, function () {
