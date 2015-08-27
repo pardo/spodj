2773f442dc53b4d862200e491d94a10a7f2bd84a
Andres Pardini <unpardos@gmail.com>
2015-08-26 22:22:41 -0300

    player queue player sync wip frontend core code

--- index.js
+++ index.js
@@ -27,8 +27,12 @@ var password = login.password;
 SpotifyPlayer = (function(){
   var that = this;
   this.initialized = false;
+  this.currentTrack = null;
   this.spotifyStream = null;
   this.throttledStream = null;
+  this.trackTimePlayed = null;
+  this.playLoopId = null;
+  this.tracksQueue = null;
 
   this.BIT_RATE = 160000; // Spotify web standard bit rate
   // Lame decoder & speaker objects
@@ -42,50 +46,89 @@ SpotifyPlayer = (function(){
     console.log("Spotify Initialized");
   });
 
+  this.isPlaying = function() {
+    return this.currentTrack != null;
+  };
+
+  this.checkAndPlay = function() {
+    if (!this.isPlaying() && this.initialized) {
+      var uri = this.tracksQueue.next();
+      if (uri != null) {
+        this.playTrackUri(uri);
+      }
+    }
+  };
+
+  this.startPlayLoop = function() {
+    this.playLoopId = setInterval(function(){
+      that.checkAndPlay()
+    }, 400);
+  };
+
   this.pause = function(){
     if (this.throttledStream != null) { this.throttledStream.pause() }
+    that.lastTime = null;
   };
 
   this.resume = function(){
     if (this.throttledStream != null) { this.throttledStream.resume() }
+    that.lastTime = null;
   };
 
-  this.stop = function(){
-    if (this.spotifyStream != null) {
-      this.spotifyStream.abort();
-      this.spotifyStream.unpipe();
-    }
+  this.skip = function(abort){
+    console.log("SKIP");
     if (this.throttledStream != null) {
       this.throttledStream.pause();
       this.throttledStream.removeAllListeners("data");
       this.throttledStream.unpipe();
     }
+
+    if (this.spotifyStream != null) {
+      this.spotifyStream.removeAllListeners("finish");
+      if (abort) {
+        this.spotifyStream.abort();  
+      }      
+      this.spotifyStream.unpipe();
+    }
+    this.currentTrack = null;
+    this.trackTimePlayed = null;
+    that.lastTime = null;
     this.spotifyStream = null;
     this.throttledStream = null;
   };
-
+ 
   this.playTrackUri = function(uri){
+    if (this.isPlaying() || !this.initialized) return;    
     console.log("Querying %s", uri);
+    this.currentTrack = uri;
     this.spotify.get(uri, function (err, track) {
-      if (err) throw err;
+      if (err) throw err;            
       console.log('Playing: %s - %s', track.artist[0].name, track.name);
-      // play() returns a readable stream of MP3 audio data
-      that.stop();
+      
+      if (this.spotifyStream != null) {
+        this.skip(true);
+      }
       that.spotifyStream = track.play();
       that.throttledStream = that.spotifyStream.pipe(new Throttle(that.BIT_RATE/8)); // convert to bytes per second
       // manually write data to the decoder stream,
-      // which is a writeable stream
+      // which is a writeable stream      
+
       that.throttledStream.on('data', function (chunk) {
+        if (that.lastTime==null) {
+          that.lastTime = (new Date()).getTime();
+        } else {
+          that.trackTimePlayed += (new Date()).getTime() - that.lastTime;
+          that.lastTime = (new Date()).getTime();
+          //console.log("Time played %s", that.trackTimePlayed/1000 );
+        }        
         that.lame.write(chunk);
-      });
-
-      //.on('finish', function () {
-      //  spotify.disconnect();
-      //});
-
+      }).on('finish', function () {      
+        setTimeout(function(){
+          that.skip();  
+        }, 100);      
+      });;
     });
   };
-
 
   return this;
 })();
@@ -94,12 +137,12 @@ TracksQueue = (function(){
     var that = this;
     this.tracksUris = [];
     //trackUris index
-    this.currentTrack = 0;
+    this.queueIndex = -1;
 
     this.shuffle = function(){};
 
     this.pushTrackUriAfterCurrent = function(uri){
-        this.tracksUris.splice(this.currentTrack+1, 0, uri);
+        this.tracksUris.splice(this.queueIndex+1, 0, uri);
     };
 
     this.pushTrackUri = function(uri){
@@ -118,11 +161,37 @@ TracksQueue = (function(){
     };
 
     this.next = function(){
-        return this.tracksUris[this.currentTrack % this.tracksUris.length]
+        this.queueIndex = (this.queueIndex+1) % this.tracksUris.length;
+        if (isNaN(this.queueIndex)) {
+          this.queueIndex = 0;
+        }
+        console.log("Current Track %s", this.queueIndex);
+        return this.tracksUris[this.queueIndex];
     };
 
     return this
 })();
+
+SpotifyPlayer.tracksQueue = TracksQueue;
+SpotifyPlayer.startPlayLoop();
+
+app.post('/play/', function(req, res){
+  var uri = req.body.uri;
+  SpotifyPlayer.playTrackUri(uri);
+  res.send("OK");
+});
+
+app.get('/track/', function(req, res){  
+  res.send({
+    uri: SpotifyPlayer.currentTrack,
+    queueIndex: TracksQueue.queueIndex,
+    timePlayed: SpotifyPlayer.trackTimePlayed
+  });
+});
+
+app.get('/queue/', function(req, res){  
+  res.send(TracksQueue.getQueue());
+});
 
 app.post('/queue/', function(req, res){
   var uri = req.body.uri;
@@ -136,20 +205,13 @@ app.post('/unqueue/', function(req, res){
   res.send("OK");
 });
 
-
-app.post('/play/', function(req, res){
-  var uri = req.body.uri;
-  SpotifyPlayer.playTrackUri(uri);
-  res.send("OK");
-});
-
 app.post('/pause/', function(req, res){  
   SpotifyPlayer.pause();
   res.send("OK");
 });
 
-app.post('/stop/', function(req, res){
-  SpotifyPlayer.stop();
+app.post('/next/', function(req, res){
+  SpotifyPlayer.skip(true);
   res.send("OK");
 });
 
--- web/js/main.js
+++ web/js/main.js
@@ -1,11 +1,43 @@
 $(function(){
-	SearchWidget = (function(){
+	AppWidget = (function(){
 		var that = this;
-		this.$el = $(".search-widget");
+		this.$el = $(".app-widget");
 		this.$input = this.$el.find("input");
 		this.$results = $(".search-results");
+		this.$albumResults = $(".album-results");
 		this.$searchBtn = $(".start-search");
+		this.$refreshQueueBtn = $('[data-resfresh-queue]');
+		this.$playerQueue = $('.player-queue');
 		
+		
+		this.uriToId = function(uri){
+			return uri.split(":")[2];
+		};
+
+		this.getAlbum = function(id){
+			return $.ajax({ 
+				url: "https://api.spotify.com/v1/albums/"+id
+			});
+		};
+
+		this.getAlbumTracks = function(id){
+			return $.ajax({ 
+				url: "https://api.spotify.com/v1/albums/"+id+"/tracks",
+				data: {
+					"ids": ids.join(","),					
+				}
+			});			
+		};
+
+		this.getTracks = function(ids){
+			return $.ajax({ 
+				url: "https://api.spotify.com/v1/tracks",
+				data: {
+					"ids": ids.join(","),					
+				}
+			});			
+		};
+
 		this.apiSearch = function(query, offset){
 			offset = offset || 0;			
 			return $.ajax({ 
@@ -28,15 +60,24 @@ $(function(){
 						tracks: response.tracks,
 					},
 					function(err, res) {
+						if (err) throw err;
 						that.$results.html(res);
 					}
 				);	
 			});			
 		};
-		
-		this.playTrackUri = function(uri){
+
+		this.queueAlbumUri = function(uri){
+			this.getAlbum(this.uriToId(uri)).done(function(album){				
+				for (var i = 0; i < album.tracks.items.length; i++) {
+					that.queueTrackUri(album.tracks.items[i].uri);
+				};
+			});
+		};
+
+		this.queueTrackUri = function(uri){
 			$.ajax({
-				url: "/play/",
+				url: "/queue/",
 				type: "post",
 				data: {
 					uri: uri
@@ -45,7 +86,37 @@ $(function(){
 		};
 
 		this.$el.on("click", '[data-track-uri]', function(){
-			that.playTrackUri($(this).data("track-uri"));
+			that.queueTrackUri($(this).data("track-uri"));
+		});
+		this.$el.on("click", '[data-album-uri]', function(){
+			that.queueAlbumUri($(this).data("album-uri"));
+		});
+
+		this.$el.on("click", '[data-album-id]', function(){
+			that.getAlbum($(this).data("album-id")).done(function(r){
+				nunjucks.render('templates/album.html', {
+					album: r
+				},
+				function(err, res) {
+					if (err) throw err;
+					that.$albumResults.html(res);
+				});
+			});			
+		});
+
+		this.$refreshQueueBtn.click(function(e){
+			e.preventDefault();
+			$.ajax({ url: "/queue/", type: "get" }).done(function(r){
+				that.getTracks(r.map(that.uriToId)).done(function(tracks){					
+					nunjucks.render('templates/queue.html', {
+						tracks: tracks.tracks
+					},
+					function(err, res) {
+						if (err) throw err;
+						this.$playerQueue.html(res);						
+					});
+				});				
+			});
 		});
 
 		this.$searchBtn.click(function(e){
@@ -53,9 +124,17 @@ $(function(){
 			that.search(that.$input.val());
 		});
 
+		this.$input.on("keydown", function(e){
+			if (e.which == 13) {
+				e.preventDefault();
+				that.search(that.$input.val());
+			}
+		});
 		
 		
+
 		return this;
+	
 	})();
 
 	
